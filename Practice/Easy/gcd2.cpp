d88a49b0d1e968d67eefbd5332a04d0bd385234e
Avinash Madasu <avinash.sai001@gmail.com>
2018-10-18 22:51:45 +0530

    Solutions

--- Practice/Easy/gcd2.cpp
+++ Practice/Easy/gcd2.cpp
@@ -1,29 +1,33 @@
-#include<iostream>
+#include<bits/stdc++.h>
+#define ll long long 
 using namespace std;
-int gcd(long int a,long int b)
+int final(ll a,ll b)
 {
-if(b==0)
-return a;
-else
-return gcd(b,a%b);
+	if(b==0)
+		return a;
+	else
+		return final(b,a%b);
+}
+int gcd(ll num,string s)
+{
+	int n=s.length(),i;
+    ll cur=0,rem;
+    for(i=0;i<n;i++)
+       	cur = (cur*10+(s[i]-'0'))%num;
+    return final(num,cur);
 }
 int main()
 {
 string s1;
-long int n,no,i,num;
+long int no,i,num;
 cin>>no;
 while(no--)
 {
-cin>>n>>s1;
-if(n!=0)
-{
-num=0;
-for(i=0;i<s1.length();i++)
-{
-num=num*10+(s1[i]-'0');
-num=num%n;
-}
-cout<<gcd(n,num)<<"\n";}
+cin>>num>>s1;
+if(num!=0)
+	cout<<gcd(num,s1)<<endl;
+else
+	cout<<s1<<endl;
 }
 return 0;
 }
